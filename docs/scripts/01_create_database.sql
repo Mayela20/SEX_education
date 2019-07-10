dc747d3334ee056822cbf58ef97965b13b8e5a2c
Mayela Bautista <maryury_bautista@hotmail.com>
2019-07-10 00:30:51 -0600

    tercer commit
    creacion de examen list

--- docs/scripts/01_create_database.sql
+++ docs/scripts/01_create_database.sql
@@ -1,3 +1,4 @@
 CREATE SCHEMA `examen` ;
-CREATE USER 'examen'@'127.0.0.1' IDENTIFIED BY 'mayela';
+-- CREATE USER 'examen'@'127.0.0.1' IDENTIFIED BY 'mayela';
+CREATE USER 'examen'@' 127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'mayela';
 GRANT ALL ON examen.* TO 'examen'@'127.0.0.1';
--- models/examendata.model.php
+++ models/examendata.model.php
@@ -3,5 +3,17 @@ require_once "libs/dao.php";
 
 // Elaborar el algoritmo de los solicitado aquí.
 
+function obtenerLista()
+{
+   $sqlstr = "select `juguetes`.`idjuguetes`,
+       `juguetes`.`nombrejuguetes`,
+       `juguetes`.`precio`,
+       `juguetes`.`estadojuguete`
+   from `examen`.`juguetes`";
+
+   $juguetes = array();
+   $juguetes = obtenerRegistros($sqlstr);
+   return $juguetes;
+}
 
 ?>
