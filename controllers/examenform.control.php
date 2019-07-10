31f129b828727c3d3030b700613dd00c975107a8
Mayela Bautista <maryury_bautista@hotmail.com>
2019-07-10 01:27:33 -0600

    cuato comming
    recta final

--- controllers/examenform.control.php
+++ controllers/examenform.control.php
@@ -3,12 +3,131 @@
 require_once "models/examendata.model.php";
 function run()
 {
+    $estadojuguetes = obtenerEstados();
+    $selectedEst = 'PLN';
+    $mode = "";
+    $errores=array();
+    $hasError = false;
+    $modeDesc = array(
+      "DSP" => "JUGUETE ",
+      "INS" => "Creando Nueva Juguete",
+      "UPD" => "Actualizando Nuevo Juguete ",
+      "DEL" => "Eliminando Juguete "
+    );
     $viewData = array();
+    $viewData["showIdJuguetes"] = true;
+    $viewData["showBtnConfirmar"] = true;
+    $viewData["readonly"] = '';
+    $viewData["selectDisable"] = '';
 
-
-
+    if (isset($_POST["xcfrt"]) && isset($_SESSION["xcfrt"]) &&  $_SESSION["xcfrt"] !== $_POST["xcfrt"]) {
+        redirectWithMessage(
+            "Petición Solicitada no es Válida",
+            "index.php?page=examenlist"
+        );
+        die();
+    }
+    $viewData["xcfrt"] = $_SESSION["xcfrt"];
+    if (isset($_POST["btnDsp"])) {
+        $mode = "DSP";
+        $moda = obtenerJuguetePorId($_POST["idjuguetes"]);
+        $viewData["showBtnConfirmar"] = false;
+        $viewData["readonly"] = 'readonly';
+        $viewData["selectDisable"] = 'disabled';
+        mergeFullArrayTo($moda, $viewData);
+        $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+    }
+    if (isset($_POST["btnUpd"])) {
+        $mode = "UPD";
+        //Vamos A Cargar los datos
+        $moda = obtenerJuguetePorId($_POST["idjuguetes"]);
+        mergeFullArrayTo($moda, $viewData);
+        $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+    }
+    if (isset($_POST["btnDel"])) {
+        $mode = "DEL";
+        //Vamos A Cargar los datos
+        $moda = obtenerJuguetePorId($_POST["idjuguetes"]);
+        $viewData["readonly"] = 'readonly';
+        $viewData["selectDisable"] = 'disabled';
+        mergeFullArrayTo($moda, $viewData);
+        $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+    }
+    if (isset($_POST["btnIns"])) {
+        $mode = "INS";
+        //Vamos A Cargar los datos
+        $viewData["modeDsc"] = $modeDesc[$mode];
+         $viewData["showIdJuguetes"]  = false;
+    }
+    // if ($mode == "") {
+    //     print_r($_POST);
+    //     die();
+    // }
+    if (isset($_POST["btnConfirmar"])) {
+        $mode = $_POST["mode"];
+        $selectedEst = $_POST["estadojuguete"];
+         mergeFullArrayTo($_POST, $viewData);
+        switch($mode)
+        {
+        case 'INS':
+            $viewData["showIdJuguetes"] = false;
+            $viewData["modeDsc"] = $modeDesc[$mode];
+            //validaciones
+            if (floatval($viewData["prcmoda"]) <= 0) {
+                $errores[] = "El precio del juguete no puede ser 0";
+                $hasError = true;
+            }
+            if (!$hasError && agregarNuevaJuguete(
+                $viewData["dscjuguete"],
+                $viewData["precjuguete"],
+                $viewData["estadojuguete"]
+            )
+            ) {
+                redirectWithMessage(
+                    "Moda Guardada Exitosamente",
+                    "index.php?page=examenlist"
+                );
+                die();
+            }
+            break;
+        case 'UPD':
+            $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+            if (modificarModa(
+                $viewData["dscjuguete"],
+                $viewData["precjuguete"],
+                $viewData["estadojuguete"],
+                $viewData["idjuguetes"]
+            )
+            ) {
+                redirectWithMessage(
+                    "Moda Actualizada Exitosamente",
+                    "index.php?page=examenlist"
+                );
+                die();
+            }
+            break;
+        case 'DEL':
+            $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+            $viewData["readonly"] = 'readonly';
+            $viewData["selectDisable"] = 'disabled';
+            if (eliminarModa(
+                $viewData["idjuguetes"]
+            )
+            ) {
+                redirectWithMessage(
+                    "Moda Eliminada Exitosamente",
+                    "index.php?page=examenlist"
+                );
+                die();
+            }
+            break;
+        }
+    }
+    $viewData["mode"] = $mode;
+    $viewData["estadojuguete"] = addSelectedCmbArray($estadojuguetes, 'cod', $selectedEst);
+    $viewData["hasErrors"] = $hasError;
+    $viewData["errores"] = $errores;
     renderizar("examenform", $viewData);
 }
-
 run();
 ?>
--- models/examendata.model.php
+++ models/examendata.model.php
@@ -16,4 +16,73 @@ function obtenerLista()
    return $juguetes;
 }
 
+function obtenerEstados()
+{
+    return array(
+        array("cod"=>"ACT", "dsc"=>"Activo"),
+        array("cod"=>"INA", "dsc"=>"Inactivo"),
+        array("cod"=>"PLN", "dsc"=>"En Planificación"),
+        array("cod"=>"RET", "dsc"=>"Retirado"),
+        array("cod"=>"SUS", "dsc"=>"Suspendido"),
+        array("cod"=>"DES", "dsc"=>"Descontinuado")
+    );
+}
+
+function obtenerJuguetePorId($id)
+{
+    $sqlstr = "select `juguetes`.`idjuguetes`,
+        `juguetes`.`nombrejuguetes`,
+        `juguetes`.`precio`,
+        `juguetes`.`estadojuguete`
+    from `examen`.`juguetes` where idjuguetes=%d";
+
+    $modas = array();
+    $modas = obtenerUnRegistro(sprintf($sqlstr, $id));
+    return $modas;
+}
+
+function agregarNuevoJuguete($dscjuguete, $prcjuguete, $estadojuguetes) {
+    $insSql = "INSERT INTO juguete(nombrejuguetes, precio, estadojuguete)
+      values ('%s', %f, %f, '%s');";
+      if (ejecutarNonQuery(
+          sprintf(
+              $insSql,
+              $dscjuguete,
+              $prcjuguete,
+              $estadojuguetes
+          )))
+      {
+        return getLastInserId();
+      } else {
+          return false;
+      }
+}
+
+function modificarJuguete($dscjuguete, $prcjuguete, $estadojuguetes, $idjuguetes)
+{
+    $updSQL = "UPDATE juguete set nombrejuguetes='%s', prcjuguete=%f,
+    estadojuguete='%s' where idjuguetes=%d;";
+
+    return ejecutarNonQuery(
+        sprintf(
+            $updSQL,
+            $dscjuguete,
+            $prcjuguete,
+            $estadojuguetes,
+            $idjuguetes
+        )
+    );
+}
+function eliminarModa($idjuguetes)
+{
+    $delSQL = "DELETE FROM juguete where idjuguetes=%d;";
+
+    return ejecutarNonQuery(
+        sprintf(
+            $delSQL,
+            $idjuguetes
+        )
+    );
+}
+
 ?>
